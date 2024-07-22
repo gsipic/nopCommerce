af52ee7846b69a7b1721fea257074ab4027b47b8
goransipic <goran.sipic@certilia.com>
2024-07-22 09:13:55 +0200

    sell car controller

---
+++ src/Presentation/Nop.Web/Themes/SaljiDalje/Controllers/SellCarController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Controllers;
+
+namespace Nop.Web.Themes.SaljiDalje.Controllers;
+
+public partial class SellCarController : BasePublicController
+{
+    public IActionResult Index()
+    {
+        return View("~/Themes/SaljiDalje/Views/SellCar/Index.cshtml");
+        //return Ok("sdfsdfsdfdfdf");
+    }
+}
