ca842c6c695108408b41ea4023b858cfccfc6ba9
goransipic <goran.sipic@certilia.com>
2024-09-05 13:54:25 +0200

    customerInfo options for adding  #2

---
+++ src/Presentation/Nop.Web/Themes/SaljiDalje/Data/SaljiDaljeConstants.cs
@@ -0,0 +1,7 @@
+namespace Nop.Web.Themes.SaljiDalje.Data;
+
+public static class SaljiDaljeConstants
+{
+    public const string AVATAR_URL = "AvatarUrl";
+    public const string NO_PROFILE_IMAGE = "/Themes/SaljiDalje/Content/img/avatars/13.png";
+}
